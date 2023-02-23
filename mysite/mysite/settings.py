5cc821bb873967612ec257ec6ee90a9540c4f002
ElahiAli <ali_elahi00@outlook.com>
2023-02-23 13:18:53 +0330

    mysql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,13 +79,23 @@ ASGI_APPLICATION = 'mysite.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql5vbp_db',
+        'USER': 'root',
+        'PASSWORD': '87p7ad2rph3p4zb',
+        'HOST':'remote.runflare.com',
+        'PORT':'30503',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
